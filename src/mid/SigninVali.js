041fb1fb04f609968a8e396efca1dc3e9fa6980f
mohamedeljak <bady.mohamed@gmail.com>
2019-06-17 16:54:46 +0200

    made code modular by dry controlls-3

--- src/mid/SigninVali.js
+++ src/mid/SigninVali.js
@@ -28,7 +28,7 @@ const schema = Joi.object().keys({
             // send a 422 error response if validation fails
             res.status(422).json({
                 status: 'error',
-                message: 'Invalid request data',
+                message: 'Invalid  Data , Please check again',
               
             });
             
